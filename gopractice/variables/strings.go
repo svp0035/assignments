7b8c1589ea7d10096814eb10395add79e36337f4
Sahil <sahilpatel4@my.unt.edu>
2022-01-03 11:46:39 -0600

    Comments added

--- gopractice/variables/strings.go
+++ gopractice/variables/strings.go
@@ -2,18 +2,21 @@ package variables
 
 import "fmt"
 
-// StringDeclaration is declaring Strings assignment
+// VarDeclarationString is declaring Strings assignment
 func VarDeclarationString() {
 
 	fmt.Println("This function will declare STRING variables")
 
+	// short hand declaration where initializatoin and declaration is done in same line
 	a, b := "Hello There", "GoLang"
 	fmt.Println(a, b)
 
+	// initalise first then declare a value
 	var c, d string
 	c, d = "Sage", "IT"
 	fmt.Println(c, d)
 
+	// initalize and declare variables
 	var e, f string = "New", "Year"
 	fmt.Println(e, f)
 
